3ef586548fc4e272a64b598cc15ac77bf74f0b6f
verticalfile30 <stopmotion45b@gmail.com>
2021-08-09 22:23:52 -0400

    inital commit for dc loader

---
+++ main.asm
@@ -0,0 +1,35 @@
+[bits 16]
+
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov bx, 0x8000
+    cli
+
+    mov ss, bx
+    mov sp, ax
+    mov bp, sp 
+    sti
+    cld
+
+    mov sp, bp
+
+    mov bx, welcome
+    call print
+    call newLine
+
+    mov cx, 0fh ;time delay
+    mov dx, 4240h
+    mov ah, 86h
+    int 15h  
+
+    mov bx, license
+    call print
+    call newLine
+
+
+
+
+welcome db "Welcome to Xenoware Diskclavier!", 0
+license db "Diskclavier is licensed under MIT", 0
+help db "Press h for help", 0
